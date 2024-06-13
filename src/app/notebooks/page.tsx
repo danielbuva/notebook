8a7feac04deaec8252cc71f177afac17b83b9adf
danielbuva <daniel.valdecantos@gmail.com>
2024-06-12 18:57:34 -0700

    feat: redirect to /notebooks after signing in

---
+++ src/app/notebooks/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+import { getSession } from "~/server/auth";
+
+export default async function Notebooks() {
+  // const session = await getSession();
+  return <div></div>;
+}
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -55,6 +55,9 @@ export const authOptions: NextAuthOptions = {
         },
       };
     },
+    redirect: ({ baseUrl }) => {
+      return baseUrl + "/notebooks";
+    },
   },
   adapter: DrizzleAdapter(db, {
     usersTable: users,
@@ -88,4 +91,4 @@ export const authOptions: NextAuthOptions = {
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => getServerSession(authOptions);
+export const getSession = () => getServerSession(authOptions);
