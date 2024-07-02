ff1d517cbad89bad5f1a083dc22f35c185500e52
danielbuva <daniel.valdecantos@gmail.com>
2024-07-01 19:48:16 -0700

    style: add styles to notebook list

--- src/app/notebooks/components/MyNotebooks.tsx
+++ src/app/notebooks/components/MyNotebooks.tsx
@@ -7,11 +7,11 @@ export default async function MyNotebooks() {
   const notebooks = await getNotebooks();
 
   return (
-    <div className="flex gap-4">
+    <div className="flex h-full w-full flex-wrap justify-start gap-x-[36px]">
       {notebooks.map((n) => (
-        <div key={n.id}>
+        <div key={n.id} className="h-72 w-44">
           <Link href={`/notebooks/${n.id}`}>
-            <div className="h-56 w-44 rounded-lg border border-[#2f3336]" />
+            <div className="h-56 rounded-lg border border-[#2f3336]" />
           </Link>
           <EditableTitle notebookId={n.id} title={n.title} />
           <DeleteNotebook id={n.id} />
